421286c03d127e773ea1093180665c480b9b47b1
Anurag Singh <contact@anurag.sh>
2022-12-28 00:04:48 +0800

    fix cond

--- src/log.rs
+++ src/log.rs
@@ -1,7 +1,7 @@
 use anyhow::Context;
 
 pub fn init_logger() {
-    let logger = fern::Dispatch::new()
+    let cfg = fern::Dispatch::new()
         .format(|out, message, record| {
             out.finish(format_args!(
                 "[{}][{}] {}",
@@ -13,19 +13,19 @@ pub fn init_logger() {
         .level(log::LevelFilter::Info);
 
     #[cfg(feature = "aws")]
-    {
-        logger
-            .level_for(
-                "aws_smithy_http_tower::parse_response",
-                log::LevelFilter::Warn,
-            )
-            .level_for(
-                "aws_config::default_provider::credentials",
-                log::LevelFilter::Warn,
-            )
-    }
-    .chain(std::io::stdout())
-    .apply()
-    .context("failed to set up logger")
-    .unwrap();
+    let cfg = {
+        cfg.level_for(
+            "aws_smithy_http_tower::parse_response",
+            log::LevelFilter::Warn,
+        )
+        .level_for(
+            "aws_config::default_provider::credentials",
+            log::LevelFilter::Warn,
+        )
+    };
+
+    cfg.chain(std::io::stdout())
+        .apply()
+        .context("failed to set up logger")
+        .unwrap();
 }
