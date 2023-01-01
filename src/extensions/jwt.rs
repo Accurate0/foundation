0a945bfd5b6c995f60ee746571741f9003ff5bba
Anurag Singh <contact@anurag.sh>
2023-01-01 16:21:52 +0800

    fix ref

--- src/extensions/jwt.rs
+++ src/extensions/jwt.rs
@@ -3,13 +3,13 @@ use aliri_oauth2::{oauth2::HasScope, Authority, AuthorityError, ScopePolicy};
 use serde::Deserialize;
 
 pub trait AliriOAuth2Extensions {
-    fn verify_token_from_str<T>(self, s: &str) -> Result<T, AuthorityError>
+    fn verify_token_from_str<T>(&self, s: &str) -> Result<T, AuthorityError>
     where
         T: for<'de> Deserialize<'de> + HasScope + jwt::CoreClaims;
 }
 
 impl AliriOAuth2Extensions for Authority {
-    fn verify_token_from_str<T>(self, s: &str) -> Result<T, AuthorityError>
+    fn verify_token_from_str<T>(&self, s: &str) -> Result<T, AuthorityError>
     where
         T: for<'de> Deserialize<'de> + HasScope + jwt::CoreClaims,
     {
