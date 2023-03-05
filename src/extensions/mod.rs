ec10308e8a5d8d2a5b9a478c13c8a6eebe95e599
Anurag Singh <contact@anurag.sh>
2023-03-05 14:03:45 +0800

    ext:wq

--- src/extensions/mod.rs
+++ src/extensions/mod.rs
@@ -1,7 +1,9 @@
 mod header_map;
 mod request;
+mod secrets_manager;
 mod verify_jwt;
 
 pub use header_map::HeaderMapExtensions;
 pub use request::RequestExtensions;
+pub use secrets_manager::SecretsManagerExtensions;
 pub use verify_jwt::AliriOAuth2Extensions;
